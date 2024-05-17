7c1e318b3e2948bb2e2cc58bc4541117997a7dcd
Japermar <japermar@yahoo.com>
2024-05-16 22:18:28 -0400

    eqwe
    eqw

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-12">
